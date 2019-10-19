1cd9a016fe6fed139d8ad928bffa0ab237085984
0916kj <0916kj@gmail.com>
2019-10-19 14:36:23 -0700

    added module and imported

--- arduino_try.py
+++ arduino_try.py
@@ -2,4 +2,8 @@
 Set up script to interface Arduino with computer
 
 Tutorial: https://www.makeuseof.com/tag/program-control-arduino-python/
+
+Use pyfirmata module
 """
+import pyfirmatafrom pyfirmata import Arduino, util
+import time
