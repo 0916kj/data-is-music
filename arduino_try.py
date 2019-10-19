a57f68e0d4ad740f92fca90b94e3f5d92dba363e
Kate James <41515657+0916kj@users.noreply.github.com>
2019-10-19 14:53:03 -0700

    Add files via upload
    edited code to stop ASCII error

--- arduino_try.py
+++ arduino_try.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Set up script to interface Arduino with computer
 
@@ -5,14 +6,15 @@ Tutorial: https://www.makeuseof.com/tag/program-control-arduino-python/
 
 Use pyfirmata module: https://github.com/tino/pyFirmata
 """
+
 from pyfirmata import Arduino, util
 import time
 
 ##Check which COM port you are using in the Arduino IDE, and enter it into your code as the variable board.
-board = Arduino("COM3")
+board = Arduino('COM3')
 
 ##Now you’ll set up the user prompt. Those familiar with Python will recognize everything here.
 ##You print a question to the screen using the input function, and store the answer as a variable.
 ##Once the user has provided a number, the program reports back how many times the LED will blink.
 loopTimes = raw_input('How many times would you like the LED to blink: ')
-print("Blinking " + loopTimes + " times.")
+print('Blinking ' + loopTimes + ' times.')
