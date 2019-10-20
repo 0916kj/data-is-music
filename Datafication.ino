46e462f6e37d8c2036bb5a432de4638731bb5742
0916kj <0916kj@gmail.com>
2019-10-20 00:05:51 -0700

    in theory should send data over usb; untested until tomorrow

--- Datafication.ino
+++ Datafication.ino
@@ -9,6 +9,11 @@ int pwm4Pin = 9;
 void setup() {
   // put your setup code here, to run once:
   pinMode(clkPin, INPUT);
+  pinMode(pwm1Pin, OUTPUT);
+  pinMode(pwm2Pin, OUTPUT);
+  pinMode(pwm3Pin, OUTPUT);
+  pinMode(pwm4Pin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -16,10 +21,10 @@ void loop() {
   clk = digitalRead(clkPin);
   if (clk != lastClk && clk == HIGH) {
     // Happens at clock pulse
-    analogWrite(pwm1Pin, random(255));
-    analogWrite(pwm2Pin, random(255));
-    analogWrite(pwm3Pin, random(255));
-    analogWrite(pwm4Pin, random(255));
+    analogWrite(pwm1Pin, Serial.read());
+    analogWrite(pwm2Pin, Serial.read());
+    analogWrite(pwm3Pin, Serial.read());
+    analogWrite(pwm4Pin, Serial.read());
   }
   lastClk = clk;
 }
