4774fa6ed4a83361ff54e0365b3cb01d604d7ab0
0916kj <0916kj@gmail.com>
2019-10-19 20:01:51 -0700

    committing last data

--- arduino_try.py
+++ arduino_try.py
@@ -1,9 +1,20 @@
+# -*- coding: utf-8 -*-
 """
 Set up script to interface Arduino with computer
 
 Tutorial: https://www.makeuseof.com/tag/program-control-arduino-python/
 
-Use pyfirmata module
+Use pyfirmata module: https://github.com/tino/pyFirmata
 """
-import pyfirmatafrom pyfirmata import Arduino, util
+
+from pyfirmata import Arduino, util
 import time
+
+##Check which COM port you are using in the Arduino IDE, and enter it into your code as the variable board.
+board = Arduino('COM3')
+
+##Now you’ll set up the user prompt. Those familiar with Python will recognize everything here.
+##You print a question to the screen using the input function, and store the answer as a variable.
+##Once the user has provided a number, the program reports back how many times the LED will blink.
+loopTimes = raw_input('How many times would you like the LED to blink: ')
+print('Blinking ' + loopTimes + ' times.')
